c87f834636e1f9ba2c7c7fb0302ff5f4164a7c53
chao3 <chao3@sigono.com>
2017-10-20 15:01:26 +0800

    Checkout before run

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -2,6 +2,9 @@
 
 def scmVars
 node() { 
+	stage("Checkout"){
+		scmVars = checkout changelog: false, poll: false, scm: [$class: 'GitSCM', branches: [[name: 'origin/master']], doGenerateSubmoduleConfigurations: false, extensions: [], submoduleCfg: [], userRemoteConfigs: [[url: 'https://github.com/sigono-chao3/test-jenkins']]]
+	}
 	stage ("First") {
 		echo 'First wow'
 		sh "$WORKSPACE/nope.sh"
